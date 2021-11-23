51e3aa8f411dcf9ae2cc1ebca6db8f2738988da0
Tim Mattick <tim.mattick@gmx.de>
2021-11-23 15:24:17 +0100

    Basic display functionality

---
+++ display.py
@@ -0,0 +1,172 @@
+"""
+from https://www.waveshare.com/w/upload/2/28/Pico_code.7z
+"""
+
+from machine import Pin,SPI,PWM
+import framebuf
+import time
+import os
+
+BL = 13
+DC = 8
+RST = 12
+MOSI = 11
+SCK = 10
+CS = 9
+
+class LCD_2inch(framebuf.FrameBuffer):
+    def __init__(self):
+        self.width = 320
+        self.height = 240
+        
+        self.cs = Pin(CS,Pin.OUT)
+        self.rst = Pin(RST,Pin.OUT)
+        
+        self.cs(1)
+        self.spi = SPI(1)
+        self.spi = SPI(1,1000_000)
+        self.spi = SPI(1,100000_000,polarity=0, phase=0,sck=Pin(SCK),mosi=Pin(MOSI),miso=None)
+        self.dc = Pin(DC,Pin.OUT)
+        self.dc(1)
+        self.buffer = bytearray(self.height * self.width * 2)
+        super().__init__(self.buffer, self.width, self.height, framebuf.RGB565)
+        self.init_display()
+        
+        self.RED   =   0x07E0
+        self.GREEN =   0x001f
+        self.BLUE  =   0xf800
+        self.WHITE =   0xffff
+        self.BLACK =   0x0000
+        self.SKY_BLUE = 0x7E9F
+        self.LIGHTER_GREY = 0xE79E
+        self.LIGHT_GREY = 0xD71D
+        
+    def write_cmd(self, cmd):
+        self.cs(1)
+        self.dc(0)
+        self.cs(0)
+        self.spi.write(bytearray([cmd]))
+        self.cs(1)
+
+    def write_data(self, buf):
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(bytearray([buf]))
+        self.cs(1)
+
+    def init_display(self):
+        """Initialize display"""  
+        self.rst(1)
+        self.rst(0)
+        self.rst(1)
+        
+        self.write_cmd(0x36)
+        self.write_data(0x70)
+
+        self.write_cmd(0x3A) 
+        self.write_data(0x05)
+
+        self.write_cmd(0xB2)
+        self.write_data(0x0C)
+        self.write_data(0x0C)
+        self.write_data(0x00)
+        self.write_data(0x33)
+        self.write_data(0x33)
+
+        self.write_cmd(0xB7)
+        self.write_data(0x35) 
+
+        self.write_cmd(0xBB)
+        self.write_data(0x19)
+
+        self.write_cmd(0xC0)
+        self.write_data(0x2C)
+
+        self.write_cmd(0xC2)
+        self.write_data(0x01)
+
+        self.write_cmd(0xC3)
+        self.write_data(0x12)   
+
+        self.write_cmd(0xC4)
+        self.write_data(0x20)
+
+        self.write_cmd(0xC6)
+        self.write_data(0x0F) 
+
+        self.write_cmd(0xD0)
+        self.write_data(0xA4)
+        self.write_data(0xA1)
+
+        self.write_cmd(0xE0)
+        self.write_data(0xD0)
+        self.write_data(0x04)
+        self.write_data(0x0D)
+        self.write_data(0x11)
+        self.write_data(0x13)
+        self.write_data(0x2B)
+        self.write_data(0x3F)
+        self.write_data(0x54)
+        self.write_data(0x4C)
+        self.write_data(0x18)
+        self.write_data(0x0D)
+        self.write_data(0x0B)
+        self.write_data(0x1F)
+        self.write_data(0x23)
+
+        self.write_cmd(0xE1)
+        self.write_data(0xD0)
+        self.write_data(0x04)
+        self.write_data(0x0C)
+        self.write_data(0x11)
+        self.write_data(0x13)
+        self.write_data(0x2C)
+        self.write_data(0x3F)
+        self.write_data(0x44)
+        self.write_data(0x51)
+        self.write_data(0x2F)
+        self.write_data(0x1F)
+        self.write_data(0x1F)
+        self.write_data(0x20)
+        self.write_data(0x23)
+        
+        self.write_cmd(0x21)
+
+        self.write_cmd(0x11)
+
+        self.write_cmd(0x29)
+
+    def show(self):
+        self.write_cmd(0x2A)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0x01)
+        self.write_data(0x3f)
+        
+        self.write_cmd(0x2B)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0xEF)
+        
+        self.write_cmd(0x2C)
+        
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(self.buffer)
+        self.cs(1)
+        
+if __name__=='__main__':
+    pwm = PWM(Pin(BL))
+    pwm.freq(1000)
+    pwm.duty_u16(32768)#max 65535
+
+    LCD = LCD_2inch()
+    #color BRG
+    LCD.fill(LCD.SKY_BLUE)
+    LCD.show()
+    time.sleep(1)
+    LCD.text("Hallo Welt", 20, 80, LCD.BLACK)
+    LCD.show()
