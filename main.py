eaf6816bff30f655e6bebd331a8d2b7c526e2ae8
Tim Mattick <tim.mattick@gmx.de>
2021-11-23 20:26:07 +0100

    reformated with black

--- main.py
+++ main.py
@@ -4,22 +4,25 @@ from display import LCD_2inch, BL
 import draw
 
 
-if __name__== '__main__':
+if __name__ == "__main__":
     pwm = PWM(Pin(BL))
     pwm.freq(1000)
-    pwm.duty_u16(32768)#max 65535
+    pwm.duty_u16(32768)  # max 65535
 
     LCD = LCD_2inch()
-    #color BRG
-        
+    # color BRG
+
     cloud_positions = [[130, 100], [50, 30], [20, 80], [200, 50], [250, 90]]
-    
-    key0 = Pin(15,Pin.IN,Pin.PULL_UP) 
-    
+
+    key0 = Pin(15, Pin.IN, Pin.PULL_UP)
+    key1 = Pin(17, Pin.IN, Pin.PULL_UP)
+    key2 = Pin(2, Pin.IN, Pin.PULL_UP)
+    key3 = Pin(3, Pin.IN, Pin.PULL_UP)
+
     while True:
         if key0.value() == 0:
             break
-        
+
         LCD.fill(LCD.SKY_BLUE)
         for x, y in cloud_positions:
             draw.draw_cloud(LCD, x, y)
@@ -27,7 +30,5 @@ if __name__== '__main__':
         for position in cloud_positions:
             position[0] += 1
             position[0] %= LCD.width
-        
+
         # time.sleep(0.5)
-    
-    
--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 def rgb_to_hex565(rgb):
     blue, red, green = rgb[0], rgb[1], rgb[2]
-    return ("%0.4X" % ((int(blue / 255 * 31) << 11) | (int(red / 255 * 63) << 5) | (int(green / 255 * 31))))
+    return "%0.4X" % (
+        (int(blue / 255 * 31) << 11)
+        | (int(red / 255 * 63) << 5)
+        | (int(green / 255 * 31))
+    )
+
 
 if __name__ == "__main__":
-    print(rgb_to_hex565([238,243,247]))
+    print(rgb_to_hex565([238, 243, 247]))
