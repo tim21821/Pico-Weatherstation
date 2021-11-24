9fed655602d8d710ca7fbfe8cd7fcb1aa9db8a2d
Tim Mattick <tim.mattick@gmx.de>
2021-11-24 09:29:22 +0100

    fixed conversion

--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
 def rgb_to_hex565(rgb):
-    blue, red, green = rgb[0], rgb[1], rgb[2]
+    """Turns a RGB triple into a hex565 BRG code
+
+    :param rgb: Triple of RGB values (0 to 255)
+    :type rgb: list
+    :return: Hex565 BRG code of the RGB color
+    :rtype: str
+    """
+    red, green, blue = rgb
     return "%0.4X" % (
         (int(blue / 255 * 31) << 11)
         | (int(red / 255 * 63) << 5)
@@ -8,4 +15,4 @@ def rgb_to_hex565(rgb):
 
 
 if __name__ == "__main__":
-    print(rgb_to_hex565([238, 243, 247]))
+    print(rgb_to_hex565([255, 255, 255]))
