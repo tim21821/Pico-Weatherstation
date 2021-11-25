05ce4daebaa03fb55348e7a433e6bb631cfe8861
Tim Mattick <tim.mattick@gmx.de>
2021-11-25 12:33:22 +0100

    added docstring

--- temperature.py
+++ temperature.py
@@ -6,7 +6,12 @@ CONVERSION_FACTOR = 3.3 / 65535
 
 
 def get_cpu_temp():
+    """Reads the CPU temperature of the Raspberry Pi Pico.
 
+    :return: The measured temperature in °C.
+    :rtype: float
+    """
+    
     # read voltage, that is proportional to temperature and convert it
     conv_voltage = TEMP_SENSOR.read_u16() * CONVERSION_FACTOR
     temp = 27 - (conv_voltage - 0.706) / 0.001721
